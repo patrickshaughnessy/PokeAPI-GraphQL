ece21f6dd5c7aba25526d855602a29efe0539f7a
Patrick Shaughnessy <pshaughn86@gmail.com>
2016-01-17 13:45:38 -0800

    remove extraneous files

--- app.js
+++ app.js
@@ -8,56 +8,12 @@ import cors from 'cors';
 
 import {Schema} from "./data/schema";
 import graphQLHTTP from "express-graphql";
-// webpack dev server for relay
-import webpack from 'webpack';
-import WebpackDevServer from 'webpack-dev-server';
 
-const APP_PORT = 3000;
-const GRAPHQL_PORT = 8080;
-
-// Expose a GraphQL endpoint
-var graphQLServer = express();
-graphQLServer.use('/graphql', graphQLHTTP({
-  graphiql: true,
-  pretty: true,
-  schema: Schema,
-}));
-// graphQLServer.listen(GRAPHQL_PORT, () => console.log(
-//   `GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}`
-// ));
-
-// Serve the Relay app
-// var compiler = webpack({
-//   entry: path.resolve(__dirname, 'js', 'main.js'),
-//   module: {
-//     loaders: [
-//       {
-//         exclude: /node_modules/,
-//         loader: 'babel',
-//         query: {stage: 0, plugins: ['./build/babelRelayPlugin']},
-//         test: /\.js$/,
-//       }
-//     ]
-//   },
-//   output: {filename: 'main.js', path: '/'}
-// });
-// var app = new WebpackDevServer(compiler, {
-//   contentBase: '/public/',
-//   // proxy: {'/graphql': `http://localhost:${GRAPHQL_PORT}`},
-//   publicPath: '/js/',
-//   stats: {colors: true}
-// });
+const APP_PORT = process.env.PORT || 3000;
 
 var app = express();
-// regular express app setup stuff:
 
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-
+app.use(favicon(path.join(__dirname, 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -65,72 +21,13 @@ app.use(cookieParser());
 app.use(cors());
 
 
-// import routes from './routes/index';
-// app.use('/', routes);
-
-// Serve static resources
-// app.use('/', express.static(path.resolve(__dirname, 'public')));
-
 app.use('/', graphQLHTTP({
   graphiql: true,
   pretty: true,
   schema: Schema,
 }));
 
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  let err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-// if (app.get('env') === 'development') {
-//   app.use((err, req, res, next) => {
-//     res.status(err.status || 500);
-//     res.render('error', {
-//       message: err.message,
-//       error: err
-//     });
-//   });
-// }
-
-// production error handler
-// no stacktraces leaked to user
-// app.use((err, req, res, next) => {
-//   res.status(err.status || 500);
-//   res.render('error', {
-//     message: err.message,
-//     error: {}
-//   });
-// });
-
 
 app.listen(APP_PORT, () => {
-  console.log(`App is now running on http://localhost:${APP_PORT}`);
+  console.log(`App is now running on port: ${APP_PORT}`);
 });
-
-
-
-
-
-// let PORT = process.env.PORT || 3000;
-
-// let app = express();
-
-// app.use(express.static(path.join(__dirname, 'public')));
-
-
-// app.use("/graphql", GraphQLHTTP({
-//   schema,
-//   graphiql: true
-// }));
-
-
-
-// app.listen(PORT, () => {
-//   console.log(`I'm listening on this port: ${PORT}`);
-// })
